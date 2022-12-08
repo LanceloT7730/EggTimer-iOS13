50868aa742b75ef7f98dbc49b88dab217032dab3
210046 <ac2952602@akfauniversity.org>
2022-12-08 22:46:55 +0500

    Completed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,24 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 1, "Medium": 2, "Hard": 3]
-    var counter:Int = 60
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 5]
+    var totalTime = 0
+    var secondsPassed = 0
     var timer = Timer()
     
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        timer.invalidate()
-        counter = eggTimes[sender.currentTitle!]! * 60
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
     }
     
-    @objc func updateCounter() {
-        if counter > 0 {
-            print("\(counter) seconds.")
-            counter -= 1
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            playSound()
         }
+    }
+    
+    var player: AVAudioPlayer!
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
 }
